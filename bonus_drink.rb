486fa0bd5312b4379ee4f6db88f0efccd3cbc561
tomoya <ihatov08@gmail.com>
2016-06-14 23:00:23 +0900

    飲める本数総計のメソッドを定義

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,14 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    count = 3
+    total = amount
+    quo_rem = amount
+    while quo_rem >= count do
+      total += quo_rem / count
+      quo = quo_rem / count
+      rem = quo_rem % count
+      quo_rem = quo + rem
+    end
+    total
   end
 end
